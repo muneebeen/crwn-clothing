5465e77621850188114c3d923e92753fa823b909
Muneeb <muneebahmed.753@gmail.com>
2022-04-04 00:22:43 +0500

    wokred on categories and category component

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./categories.scss";
+import Categories from "./Components/Categories/Categories";
+
+const App = () => {
+  return <Categories></Categories>;
+};
+export default App;
---
+++ src/Components/CategoryItem/Category.js
@@ -0,0 +1,20 @@
+import "./Category.scss";
+
+const Category = (props) => {
+  const { title, id, imageUrl } = props.category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+export default Category;
